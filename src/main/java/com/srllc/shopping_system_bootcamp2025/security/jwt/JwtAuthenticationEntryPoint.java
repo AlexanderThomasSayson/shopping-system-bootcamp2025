a231b756c2b38618173a7db24d86a345831ce613
Alexander Thomas Sayson <alexander@solutionsresource.com>
2025-09-02 16:03:29 +0800

    feat: add JWT filters and generators.

---
+++ src/main/java/com/srllc/shopping_system_bootcamp2025/security/jwt/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,23 @@
+package com.srllc.shopping_system_bootcamp2025.security.jwt;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    // Basically send an error once an invalid or no token is received from the request.
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+    }
+}
