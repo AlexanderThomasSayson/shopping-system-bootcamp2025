82c1aac8d3f838ae3b63ad61211d679e88ccc689
Alexander Thomas Sayson <alexander@solutionsresource.com>
2025-09-02 16:00:58 +0800

    feat: firstname and lastname field.

--- src/main/java/com/srllc/shopping_system_bootcamp2025/domain/entity/User.java
+++ src/main/java/com/srllc/shopping_system_bootcamp2025/domain/entity/User.java
@@ -33,14 +33,21 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
     @Column(nullable = false, unique = true)
     private String userName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
 
     @Column(nullable = false)
     private String password;
 
-    @Column(nullable = false, unique = true)
-    private String email;
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "user_roles",
