53cba3375752fb9288ef3f34e3b04e8f01e0a6b5
Alexander Thomas Sayson <alexander@solutionsresource.com>
2025-09-02 13:57:41 +0800

    initial commit

---
+++ src/test/java/com/srllc/shopping_system_bootcamp2025/ShoppingSystemBootcamp2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srllc.shopping_system_bootcamp2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingSystemBootcamp2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
